470b5e181f93b60863d7f98572c6e0fbbbeb5311
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-11-19 14:14:12 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import hero from '@/components/hero'
+import Hero from '@/components/hero'
 import Project from '@/components/Project'
 import Skill from '@/components/skill'
 import Footer from '@/components/Footer'
@@ -8,7 +8,7 @@ import Certifi from '@/components/certifi'
 export default function Home() {
   return (
    <div>
-    <hero/>
+    <Hero/>
     <Project/>
     <Skill/>
     <Certifi/>
