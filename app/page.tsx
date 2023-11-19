4c4935bc4ccb7d9a5e6faa17ad802218b0040b10
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-11-19 16:01:55 +0530

    UPDATE

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import Hero from '@/components/Hero'
 import Project from '@/components/Project'
 import Skill from '@/components/Skill'
--- components/Project.tsx
+++ components/Project.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Projectcont from './Projectcont'
-import Projectcont from '@/components/Projectcont'
+// import Projectcont from '@/components/Projectcont'
 const Project = () => {
     return (
         <div className="hero min-h-screen bg-black">
@@ -9,11 +9,11 @@ const Project = () => {
                     <h1 className="lg:text-5xl text-5xl  max-md:text-4xl max-sm:text-4xl font-black ">PROJECTS</h1>
                     <div
                         className=' w-full flex flex-wrap justify-center'>
-                        <Projectcont />
+                        {/* <Projectcont />
                     <Projectcont />
                     <Projectcont />
                     <Projectcont />
-                    <Projectcont />
+                    <Projectcont /> */}
 
                         <div className="card  bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-indigo-500 bg-opacity-25  m-2 w-fit">
                             <div className="card-body">
