ad8d802f2fb9b5a2138e1b7870870b060f15fc88
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-11-19 18:30:49 +0530

    update

--- components/COMP/Deveop.tsx
+++ components/COMP/Deveop.tsx
@@ -6,23 +6,19 @@ const Deveop = () => {
       <div
         className='flex w-fit   justify-center  flex-col flex-wrap font-black items-center'>
         <div>
-          <div className='lg:text-5xl text-4xl  max-md:text-3xl max-sm:text-xl'>
-            Hai I{"'"}m
-          </div>
+        <div className='lg:text-5xl text-5xl  max-md:text-3xl max-sm:text-xl font-black text-white text-center'>
+                        Hai I{"'"}m
+                    </div>
 
           <div
-            className='text-primary    lg:text-9xl text-8xl max-md:text-6xl max-sm:text-5xl  flex-col flex  font-black w-fit'>
-            FULL STACK
-            <span
-              className=''>DEVELOPER</span>
+            className='text-primary text-center   lg:text-9xl text-8xl max-md:text-6xl max-sm:text-5xl  flex-col flex  font-black w-fit'>
+            FULL STACK DEVELOPER
           </div>
         </div>
 
         <p
           className=' w-11/12 text-center break-words  align-middle text-white '>
-          As a full-stack web developer , I am well-versed in a wide range of programming languages and technologies.
-          Currently, I am pursuing a B.Tech in Computer Science and Business Systems at KIT- Kalaignarkarunanidhi Institute of Technology,
-          where I have gained a solid foundation in software engineering principles and best practices. I am passionate about collaborating with industry professionals to further develop my skills and knowledge, and I am excited about the opportunities that lie ahead.
+         Hey there! I{"'"}m a passionate full-stack developer deeply interested in crafting intuitive user interfaces and robust server-side solutions. With hands-on experience in both front-end and back-end technologies, I bring a comprehensive approach to development. Let{"'"}s turn ideas into reality!
         </p>
       </div>
     </div>
