8461bdf4e29d5add49bebc4a1dcd83a472011bf7
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-11-19 09:39:39 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,10 @@ import Image from 'next/image'
 import Header from '@/components/Header'
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-200">
-  
-  <div className="diff  h-full ">
-  <div className="diff-item-1 ">
-    {/* <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">DAISY</div> */}
-    dev
-    <div
-    className='text-black bg-teal-800  text-9xl justify-center flex items-center font-black'>
-      hello
-    </div>
-  </div>
-  <div className="diff-item-2 ">
-    <div className="bg-red-900 text-primary-content text-9xl font-black grid place-content-center">DEVELOPER</div>
-  </div>
-  <div className="diff-resizer"></div>
-</div>
+   <div>
+    <Header/>
+    <Header/>
+   </div>
 
-  
-</div>
-    
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="diff h-full   ">
+        <div className="diff-item-1 ">
+
+          <div className='bg-black  justify-center flex flex-col flex-wrap font-black items-center '>
+            <div
+              className=' flex w-fit justify-center  flex-col flex-wrap font-black '>
+              <div className='text-5xl'>
+                I{"'"}m
+              </div>
+
+              <div
+                className=' text-secondary   text-9xl  flex-col flex  font-black w-fit'>
+                DHANUSH
+                <span
+                  className=''>PRABAKARAN</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="diff-item-2 ">
+          <div className=" bg-secondary text-primary-content text-9xl font-black grid place-content-center">
+            
+            <div
+            className=' flex w-fit justify-center  flex-col flex-wrap font-black '>
+            <div className='text-5xl'>
+              I{"'"}m
+            </div>
+
+            <div
+              className=' text-primary   text-9xl  flex-col flex  font-black w-fit'>
+              FULL STACK
+              <span
+                className=''>DEVELOPER</span>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div className="diff-resizer"></div>
+      </div>
+    </div>
   )
 }
 
