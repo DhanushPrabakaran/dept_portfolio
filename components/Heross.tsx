e4a2162a2388b14d18abde8f4ca5f43c72549808
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-11-23 23:39:41 +0530

    update

--- components/Heross.tsx
+++ components/Heross.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
 // import Dname from './dname'
 // import Deveop from './deveop'
-import Dname from './COMP/Dname'
-import Deveop from './COMP/Deveop'
+import Dname from './Dname'
+import Deveop from './Deveop'
 const Heross = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-    
-    {/* // 
-    //   <div className="diff h-full aspext[16:9]   ">
-    //     <div className="diff-item-1  ">
-    //       <Deveop />
-    //     </div>
-    //     <div className="diff-item-2 "> */}
-          <Dname />
-        {/* </div>
-        <div className="diff-resizer"></div>
-      </div>
-    </div> */}
+          <Dname />      
     </div>
   )
 }
